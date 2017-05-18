e04d5edfcacc668368fa76bde76ab1ec1d6f4cfe
yagosenhorini <yagosenhorini@gmail.com>
2017-05-18 14:25:36 -0300

    Criação do SpringBoot

--- src/main/java/br/com/estudo/erro/SpringBootWebApplication.java
+++ src/main/java/br/com/estudo/erro/SpringBootWebApplication.java
@@ -1,5 +1,13 @@
 package br.com.estudo.erro;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringBootWebApplication {
 
+	public static void main (String []args) throws Exception{
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+	
 }
